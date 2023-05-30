0d6a6eab8f909b85db6ea924cd31bcf3b66ae52e
Mordzifah99 <mordzifahbenedicta@gmail.com>
2023-05-30 09:25:52 -0700

    task 7

--- length_modifiers_specifier.c
+++ length_modifiers_specifier.c
@@ -1,49 +1,36 @@
 #include "main.h"
-/**
- * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
- * @ind: ind.
- * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: 1 0r 2
- */
-int  handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-	int flags, int width, int precision, int size)
-{
-	int k, unknow_len = 0, printed_chars = -1;
-	fmt_t fmt_types[] = {
-		{'c', print_char}, {'s', print_string}, {'%', print_percent},
-		{'k', print_int}, {'d', print_int}, {'b', print_binary},
-		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
-		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
-		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
-	};
-	for (k = 0; fmt_types[k].fmt != '\0'; k++)
-		if (fmt[*ind] == fmt_types[k].fmt)
-			return (fmt_types[k].fn(list, buffer, flags, width, precision, size));
 
-	if (fmt_types[k].fmt == '\0')
+/**
+ * get_width - Calculates the width for printing
+ * @format: Formatted string in which to print the arguments.
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: width.
+ */
+int get_width(const char *format, int *k, va_list list)
+{
+	int curr_k;
+	int width = 0;
+
+	for (curr_k = *k + 1; format[curr_k] != '\0'; curr_k++)
 	{
-		if (fmt[*ind] == '\0')
-			return (-1);
-		unknow_len += write(1, "%%", 1);
-		if (fmt[*ind - 1] == ' ')
-			unknow_len += write(1, " ", 1);
-		else if (width)
+		if (is_digit(format[curr_k]))
 		{
-			--(*ind);
-			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
-				--(*ind);
-			if (fmt[*ind] == ' ')
-				--(*ind);
-			return (1);
+			width *= 10;
+			width += format[curr_k] - '0';
 		}
-		unknow_len += write(1, &fmt[*ind], 1);
-		return (unknow_len);
+		else if (format[curr_k] == '*')
+		{
+			curr_k++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+			break;
 	}
-	return (printed_chars);
+
+	*k = curr_k - 1;
+
+	return (width);
 }
